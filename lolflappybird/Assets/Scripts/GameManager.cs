c0d80c9dd2177ff06fd13493569a47cb161a943c
Sahal <68587471+SahalDev@users.noreply.github.com>
2020-09-29 11:52:00 +0400

    lol

---
+++ lolflappybird/Assets/Scripts/GameManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Video;
+
+public class GameManager : MonoBehaviour
+{
+    public GameObject gameoverCanvas;
+
+    private void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    public void GameOver()
+    {
+        gameoverCanvas.SetActive(true);
+        Time.timeScale = 0;
+    }
+
+    public void Replay()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
---
+++ lolflappybird/Assets/Scripts/MainMenuToGamelol.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenuToGamelol : MonoBehaviour
+{
+    public void game()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
